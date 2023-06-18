7a3818b4d2b157a9216878e01aa59fcb24008095
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-18 23:01:51 +0530

    Prime number from 0-1000

---
+++ PrintPrimeNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    internal class PrintPrimeNumbers
+    {
+        public void PrintPrimeNumber(int start, int end)
+        {
+            for (int number = start; number <= end; number++)
+            {
+                if (IsPrime(number))
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithmProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime numbers between 0 and 1000:");
+            PrintPrimeNumbers pn = new PrintPrimeNumbers();
+                pn.PrintPrimeNumber(0, 1000);
+        }
+    }
+}
