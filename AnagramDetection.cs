3124ac065e3a2395da26271d9ec9913fda6e5ee9
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-19 22:46:35 +0530

    Anagram detection added

---
+++ AnagramDetection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    internal class AnagramDetection
+    {
+        public bool AreAnagrams(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+                return false;
+
+            char[] charArray1 = str1.ToLower().ToCharArray();
+            char[] charArray2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+
+            for (int i = 0; i < charArray1.Length; i++)
+            {
+                if (charArray1[i] != charArray2[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,24 @@
             Console.WriteLine("Prime numbers between 0 and 1000:");
             PrintPrimeNumbers pn = new PrintPrimeNumbers();
                 pn.PrintPrimeNumber(0, 1000);
+
+
+
+            /// anagram detection
+            AnagramDetection anagramDetection = new AnagramDetection();
+            string str1 = "listen";
+            string str2 = "silent";
+
+
+            if (anagramDetection.AreAnagrams(str1, str2))
+            {
+                Console.WriteLine("The two strings are anagrams.");
+            }
+            else
+            {
+                Console.WriteLine("The two strings are not anagrams.");
+            }
         }
+    }
     }
 }
